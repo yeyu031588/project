0ec4af386f901d210ea57f043f72580d97d09895
yy <yy@mks.com>
2015-12-30 16:34:50 +0800

    s

---
+++ app/Http/Controllers/Public/UploadController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Validator;
+use URL;
+use Redirect;
+class UploadController extends Controller
+{
+   
+    
+   
+
+
+}
