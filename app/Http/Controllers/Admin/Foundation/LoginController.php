b99a37e733a736c82f72cb2c6f77b6a781985940
musicsnap <musicsnap@163.com>
2016-01-07 20:07:18 +0800

    no message

---
+++ app/Http/Controllers/Admin/Foundation/LoginController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 16-1-6
+ * Time: 下午10:43
+ */
+namespace App\Http\Controllers\Admin\Foundation;
+use App\Http\Controllers\Admin\Controller;
+class LoginController extends Controller
+{
+
+}
