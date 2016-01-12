446bde631f7b59f981fac52ab917db0de0953310
music <musicsnap@163.com>
2016-01-12 17:46:48 +0800

    1

--- public/index.php
+++ public/index.php
@@ -46,13 +46,28 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | and wonderful application we have prepared for them.
 |
 */
-
+//解析Illuminate\Contracts\Http\Kernel，实例化App\Http\Kernel
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
+//a.构造函数：设置$app/$router，初始化$router中middleware数值
+//handle处理请求 —— 经过路由发送请求：
+/**
+ * $request是经过Symfony封装的请求对象
+ * 注册request实例到容器 ($app[‘request’]->Illuminate\Http\Request)
+ * 清空之前容器中的request实例
+ * 调用bootstrap方法，启动一系列启动类的bootstrap方法：
+ *      1.Illuminate\Foundation\Bootstrap\DetectEnvironment 环境配置（$app[‘env’]）
+ *      2.Illuminate\Foundation\Bootstrap\LoadConfiguration  基本配置（$app[‘config’]）
+ *      3.Illuminate\Foundation\Bootstrap\ConfigureLogging   日志文件（$app[‘log’]）
+ *      4.Illuminate\Foundation\Bootstrap\HandleExceptions   错误&异常处理
+ *      5.Illuminate\Foundation\Bootstrap\RegisterFacades    清除已解析的Facade并重新启动，注册config文件中alias定义的所有Facade类到容器
+ *      6.Illuminate\Foundation\Bootstrap\RegisterProviders  注册config中providers定义的所有Providers类到容器
+ *      7.Illuminate\Foundation\Bootstrap\BootProviders      调用所有已注册Providers的boot方法
+ * 通过Pipeline发送请求，经过中间件，再由路由转发，最终返回响应
+ */
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
 
-$response->send();
+$response->send();//将响应信息发送到浏览器：
 
-$kernel->terminate($request, $response);
+$kernel->terminate($request, $response);//处理继承自TerminableMiddleware接口的中间件（Session）并结束应用生命周期：
